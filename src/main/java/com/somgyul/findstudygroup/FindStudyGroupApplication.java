5101a083d75a9c7a63e02ce4a9cbb26fd62094fc
LeeSomgyul <rmawl8600@naver.com>
2024-12-20 17:15:37 +0900

    Initial commit

---
+++ src/main/java/com/somgyul/findstudygroup/FindStudyGroupApplication.java
@@ -0,0 +1,13 @@
+package com.somgyul.findstudygroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindStudyGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindStudyGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somgyul/findstudygroup/FindStudyGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somgyul.findstudygroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindStudyGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
